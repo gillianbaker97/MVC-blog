f226d77ec1ef6d27524bf79de632d46c0ed9a9fd
Gillian Baker <gillianbaker97@gmail.com>
2021-08-22 15:41:12 -0400

    new

--- Develop/controllers/api/projectRoutes.js
+++ Develop/controllers/api/projectRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { Project } = require('../../models');
 // route for comments under post
 
+// post new content
 router.post('/', async (req, res) => {
   try {
     const newProject = await Project.create({
@@ -15,6 +16,7 @@ router.post('/', async (req, res) => {
   }
 });
 
+// delete new content
 router.delete('/:id', async (req, res) => {
   try {
     const projectData = await Project.destroy({
